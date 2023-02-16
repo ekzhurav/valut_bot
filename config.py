6b3d51029d448e831bfc94cdfbb3802936dfe48f
ekzhurav <119040858+ekzhurav@users.noreply.github.com>
2023-02-16 10:27:41 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = '6062495025:AAEKdDzyikrrrnhBUIqs9J3A4V0G86RtHDg'
+keys = {'биткоин':'BTC',
+        'эфириум': 'ETH',
+        'доллар': 'USD',
+        'рубль': 'RUB',
+        'евро': 'EUR'
+        }
